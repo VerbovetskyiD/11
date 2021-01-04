64ca292a024a50ac6bb9201eff7e300ce30470ed
Dmitry Verbovetskyi <verbovetskyi.d@gmail.com>
2021-01-04 12:07:05 +0200

    h/w v2

--- code.js
+++ code.js
@@ -1,17 +1,8 @@
-function bind(fn, context, ...args) { //rest принимает дополнительные параметры
-
-    //реализовывая bind, нам нужно вызвать метод в контексте объекта context
-    //поэтому, для начала, возвращаем функцию fn
-    return function (...restArgs) { //restArgs принимает параметры возвращаемой функции
-        const id = Date.now().toString(); //уникальный ключ для метода (можно использовать Symbol?)
-        context[id] = fn; //связываем по id fn и context
-
-        const result = context[id]( ...args.concat(restArgs)); //передаём остальные параметры и вызываем функцию
-        delete context[id]; //удаляем ключ из объекта
-        return result; //возвращаем результат
+function bind(fn, context, ...args) {
+    return function (...restArgs) {
+        return fn.call(context, ...args, ...restArgs)
     }
 }
-
 //проверка
 const person = {
     name: 'NoName'
